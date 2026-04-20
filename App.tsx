efd5e16548001f6210b556193e72818477dafe93
lucam <lucamig2004@gmail.com>
2026-04-20 15:29:59 +0200

    dmg update

--- App.tsx
+++ App.tsx
@@ -176,7 +176,7 @@ const App: React.FC = () => {
   }, []);
 
   const handleDownload = () => {
-    window.location.href = 'https://cdn.feather-editor.it/Feather-Stable.zip';
+    window.location.href = 'https://cdn.feather-editor.it/Feather-Stable.dmg';
   };
 
   return (
